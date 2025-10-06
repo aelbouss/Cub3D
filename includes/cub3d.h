e4646759d1dae2251380f1cb1c7471b1a4af4599
Anass Elboussaili <aelbouss@c4r3p2.1337.ma>
2025-10-06 14:28:28 +0100

    returns back the correct version

---
+++ includes/cub3d.h
@@ -0,0 +1,81 @@
+# ifndef CUB3D_H
+# define CUB3D_H
+
+// headers
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <mlx.h>
+
+
+//macros  section
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+// initerfaces
+
+// macros section 
+
+# define	TILESIZE 64
+# define	POV	60
+# define	RES 4
+# define RED 0xFF0000
+
+typedef	struct s_game
+{
+	char	**map;
+	void	*mlx;
+	void	*mlx_win;
+	int		map_h;
+	int		map_w;
+}	t_game;
+
+typedef	struct s_img
+{
+	int	bpp;
+	int	size_line;
+	int	endian;
+	char	*img_data;
+	void	*wold2d;
+}	t_img;
+
+typedef	struct s_player
+{
+	int	p_x;
+	int	p_y;
+	int	fov;
+	int pos;
+	int	dir;
+}	t_player;
+
+typedef	struct s_box
+{
+	t_game	*cub;
+	t_img	*img;
+	t_player	*plyr;
+}	t_box;
+
+// prototypes section
+size_t	ft_strlen(char *str);
+int	loking_for_newline(const char *str);
+char	*elicit_line(char *str);
+char	*get_next_line(int fd);
+char	*copy_remaining(char *str, int i);
+char	*get_remaining(char *str);
+char	**read_map(char *file);
+t_box	*setup_environement(char *mapfile);
+int	build_dependencies(t_box *box);
+int	allocate_sprites(t_box *box);
+void	extract_map_width_hight(t_box *box);
+void	draw_player (t_box *box);
+void	put_pixel_to_image(char *img_data, int x, int y, int color, int bpp, int size_line);
+void	fill_2d_world(t_box *box);
+int	push_pixels_to_image(t_box *box, void *image, int color, int y, int x);
+int	draw_2d_world(t_box *box);
+int	loking_for_wall(int x, int y, t_box *box);
+int	key_handler (int keycode, t_box *box);
+
+# endif
---
+++ srcs/cub3d.c
@@ -0,0 +1,119 @@
+#include "../includes/cub3d.h"
+
+
+void	draw_player (t_box *box)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+
+	while (y < 20)
+	{
+		x = 0; 
+		while (x < 20)
+		{
+			mlx_pixel_put(box->cub->mlx, box->cub->mlx_win, box->plyr->p_x + x, box->plyr->p_y + y, RED);
+			x++;
+		}
+		y++;
+	}
+}
+
+int	key_handler (int keycode, t_box *box)
+{
+	if (keycode == 65362)
+	{
+		if (loking_for_wall(box->plyr->p_x, box->plyr->p_y - 5, box) != 1)
+		{
+			box->plyr->p_y -= 10 ;
+			draw_2d_world(box);
+			draw_player(box);
+		}
+	}
+	if (keycode == 65364)
+	{
+		if (loking_for_wall(box->plyr->p_x, box->plyr->p_y + 5, box) != 1)
+		{
+			box->plyr->p_y += 10 ;
+			draw_2d_world(box);
+			draw_player(box);
+		}
+	}
+	if (keycode == 65361)
+	{
+		if (loking_for_wall(box->plyr->p_x - 10, box->plyr->p_y, box) != 1)
+		{
+			box->plyr->p_x -= 10 ;
+			draw_2d_world(box);
+			draw_player(box);
+		}
+	}
+	if (keycode == 65363)
+	{
+		if (loking_for_wall(box->plyr->p_x + 10, box->plyr->p_y, box) != 1)
+		{
+			box->plyr->p_x += 10 ;
+			draw_2d_world(box);
+			draw_player(box);
+		}
+	}
+
+	return (0);
+}
+
+
+int	draw_2d_world(t_box *box)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (box->cub->map[y])
+	{
+		x = 0;
+		while (box->cub->map[y][x])
+		{
+			if (box->cub->map[y][x] == '1')
+				push_pixels_to_image(box, box->img->wold2d, 0x565656, y * TILESIZE, x * TILESIZE);
+			if (box->cub->map[y][x] == '0')
+				push_pixels_to_image(box, box->img->wold2d, 0xFFFFFF, y * TILESIZE, x * TILESIZE);
+				// draw white color ;
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(box->cub->mlx, box->cub->mlx_win,box->img->wold2d , 0, 0);
+	return (0);
+}
+
+
+
+int	loking_for_wall(int x, int y, t_box *box)
+{
+	if (box->cub->map[(int)(y / TILESIZE)][(int )(x / TILESIZE)] == '0')
+		return (0);
+	return (1);
+}
+
+
+int	main(int ac, char **av)
+{
+	t_box	*box;
+
+	if (ac != 2)
+		return (perror("Error : Invalid Arguments\n"), 1);
+	box = setup_environement(av[1]);
+	if (!box)
+		return (1);
+	if (build_dependencies(box) != 0)
+		return (1);
+	draw_2d_world(box);
+	draw_player(box);
+	mlx_key_hook(box->cub->mlx_win, key_handler, box);
+	mlx_loop(box->cub->mlx);
+	return (0);
+}
+
+
+// checks the  map boundaries  too god  draw  ray 
