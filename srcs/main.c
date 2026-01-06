d47b51ea7e9ef4c1eda2ace8658a5819e11f51e3
Rida Maanane <ridamaanane@gmail.com>
2026-01-06 19:23:15 +0100

    adding cross exit

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,13 @@
 //# include "../includes/cub3d.h"
 # include "../includes/game.h"
 
+int	cross_exit(t_game *game)
+{
+	mlx_destroy_window(game->engine->mlx, game->engine->mlx_win);
+	ft_putendl_fd("X detected", 2);
+	exit(0);
+}
+
 int	main(int ac, char **av)
 {
 	t_game	*game;
@@ -27,10 +34,10 @@ int	main(int ac, char **av)
 	// cast_rays(game);
 	// Key press event (key down)
 	cast_3d_walls(game);
+	mlx_hook(game->engine->mlx_win, 17, 0, cross_exit, game); //17 “window close button clicked” → 0 no mask needed
 	mlx_hook(game->engine->mlx_win, 2, 1L << 0, is_pressed, game);
 	mlx_hook(game->engine->mlx_win, 3, 1L << 1, is_released, game);
 	mlx_loop_hook(game->engine->mlx, handle_input, game);
-	
 	mlx_loop(game->engine->mlx);
 	return (0);
 }
